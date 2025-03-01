31352094e33e3783bff35b9bf1571e5608cb82ab
krishh26 <kishansinhparmar999@gmail.com>
2025-03-01 18:55:09 +0530

    changes done

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
 
   url: "http://localhost:4200",
   //  baseUrl : 'http://192.168.1.7:4100' ,
-  baseUrl: 'http://192.168.198.153:3006'
+  baseUrl: 'https://api.delphiservices.co.uk'
 };

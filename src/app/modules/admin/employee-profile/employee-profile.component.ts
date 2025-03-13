dafbb83d027d63d1e8c76faef38d516cc08be161
krishh26 <kishansinhparmar999@gmail.com>
2025-03-14 03:08:30 +0530

    changes done in user profile

--- src/app/modules/admin/employee-profile/employee-profile.component.ts
+++ src/app/modules/admin/employee-profile/employee-profile.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthServiceService } from 'src/app/services/auth/auth-service.service';
+import { LocalStorageService } from 'src/app/services/local-storage/local-storage.service';
+import { NotificationService } from 'src/app/services/notification/notification.service';
 
 @Component({
   selector: 'app-employee-profile',
@@ -6,5 +10,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./employee-profile.component.css']
 })
 export class EmployeeProfileComponent {
+
+  loginUser: any;
+  token!: any;
+  constructor(
+    private authServiceService: AuthServiceService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private notificationService: NotificationService,
+    private localStorageService: LocalStorageService
+  ) {
+    this.loginUser = this.localStorageService.getLogger();
+  }
+
+  ngOnInit(): void {
+    this.token = this.loginUser?.email;
+    this.loginUser = this.localStorageService.getLogger();
+  }
 
 }
